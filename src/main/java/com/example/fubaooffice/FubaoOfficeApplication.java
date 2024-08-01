6801f5427aa1a3135a75203141424ba932e35a62
YunSangHyeok <yshg98@naver.com>
2024-08-01 22:42:15 +0900

    initial commit

---
+++ src/main/java/com/example/fubaooffice/FubaoOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fubaooffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FubaoOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FubaoOfficeApplication.class, args);
+    }
+    // test
+}
